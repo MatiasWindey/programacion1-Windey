1fe7762d2ced40852e61a25e1e94163e063c251a
MatiasWindey <MatiasWindey4@gmail.com>
2025-05-02 12:30:26 -0300

    commit 2

--- Entrega2/Entrega2/Entrega2/Program.cs
+++ Entrega2/Entrega2/Entrega2/Program.cs
@@ -1,3 +1,27 @@
-﻿Random random = new Random(100);
+﻿
+//  Carga un array con 10 números aleatorios entre 1 y 100. Mostrar el mayor y el menor.
 
+int menor = 100; 
+int mayor = 0;
+
+int[] numeros = new int[10];
+
+Random random = new Random();
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    numeros[i] = random.Next(101);
+   
+    Console.WriteLine(numeros[i]);
+
+    if (numeros[i] > mayor)
+    {
+        mayor = numeros[i];
+    }
+    if (numeros[i] < menor)
+    {
+        menor = numeros[i];
+    }
+}
+Console.WriteLine($"el mayor es  {mayor} y el menor es  {menor}");
 
--- Entrega2/Entrega2/Entrega2/obj/Debug/net8.0/Entrega2.AssemblyInfo.cs
+++ Entrega2/Entrega2/Entrega2/obj/Debug/net8.0/Entrega2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entrega2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1380fb5233317fc10438abafbcd09ca7f080c1a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+654267533a54869f148129e1fa080e7e904ecadb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entrega2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entrega2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
