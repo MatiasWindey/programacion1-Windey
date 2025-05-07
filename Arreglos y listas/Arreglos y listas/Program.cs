fabed33cc9a54240ab18c321b414189035748aad
MatiasWindey <MatiasWindey4@gmail.com>
2025-05-07 01:24:44 -0300

    Commit arreglos

---
+++ Arreglos y listas/Arreglos y listas/Program.cs
@@ -0,0 +1,27 @@
+﻿
+/*
+
+//1. Dado un array de enteros, mostrar la suma total de sus elementos.
+
+using System.Diagnostics.CodeAnalysis;
+
+int suma = 0;
+
+int[] arreglo = new int[7];
+
+for (int i = 0; i < arreglo.Length; i++)
+{
+    Console.WriteLine("ingrese un valor");
+    if(int.TryParse(Console.ReadLine(), out arreglo[i]))
+    {
+
+        suma += arreglo[i];
+
+    }
+
+}
+
+Console.WriteLine(String.Join(",", arreglo));
+Console.WriteLine(suma);
+
+*/
